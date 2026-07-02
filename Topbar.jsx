8e529af1bf80269206689106d5be4a177a6bd75d
jjaquez123849 <joeljaqu123@gmail.com>
2026-07-02 18:38:11 -0400

    Create Topbar.jsx

---
+++ Topbar.jsx
@@ -0,0 +1,12 @@
+function Topbar() {
+  return (
+    <header className="topbar">
+      <div>
+        <strong>Centro de Operaciones</strong>
+        <span> Plataforma configurable BPM + IA documental</span>
+      </div>
+    </header>
+  );
+}
+
+export default Topbar;

1178eb720316bf592d66a352a1cd54903408d827
jjaquez123849 <joeljaqu123@gmail.com>
2026-07-03 13:14:57 -0400

    Update registerDefaultWidgets.js

--- registerDefaultWidgets.js
+++ registerDefaultWidgets.js
@@ -3,9 +3,15 @@ import { registerWidget } from "./WidgetRegistry.js";
 import AIWidget from "../widgets/AIWidget.jsx";
 import TimelineWidget from "../widgets/TimelineWidget.jsx";
 import LogWidget from "../widgets/LogWidget.jsx";
+import DocumentsWidget from "../widgets/DocumentsWidget.jsx";
+import TasksWidget from "../widgets/TasksWidget.jsx";
+import QualityWidget from "../widgets/QualityWidget.jsx";
 
 export function registerDefaultWidgets() {
   registerWidget("AI_SUMMARY", AIWidget);
   registerWidget("TIMELINE", TimelineWidget);
   registerWidget("LOG", LogWidget);
+  registerWidget("DOCUMENTS", DocumentsWidget);
+  registerWidget("TASKS", TasksWidget);
+  registerWidget("QUALITY", QualityWidget);
 }

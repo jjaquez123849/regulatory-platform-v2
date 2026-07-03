69c491542091ef1bda395c2957824277b07b0555
jjaquez123849 <joeljaqu123@gmail.com>
2026-07-03 10:43:53 -0400

    Add DocumentUnderstandingPage route to AppRoutes

--- AppRoutes.jsx
+++ AppRoutes.jsx
@@ -6,6 +6,7 @@ import DashboardPage from "../../features/dashboard/DashboardPage.jsx";
 import LogPage from "../../features/log/LogPage.jsx";
 import RecordsPage from "../../features/records/RecordsPage.jsx";
 import DocumentsPage from "../../features/documents/DocumentsPage.jsx";
+import DocumentUnderstandingPage from "../../features/documents/DocumentUnderstandingPage.jsx";
 import ExtractionResultsPage from "../../features/extraction/ExtractionResultsPage.jsx";
 import TasksPage from "../../features/tasks/TasksPage.jsx";
 import QualityPage from "../../features/quality/QualityPage.jsx";
@@ -26,8 +27,11 @@ function AppRoutes() {
         <Route path="/" element={<DashboardPage />} />
         <Route path="/log" element={<LogPage />} />
         <Route path="/records" element={<RecordsPage />} />
+
         <Route path="/documents" element={<DocumentsPage />} />
+        <Route path="/documents/:documentId/understanding" element={<DocumentUnderstandingPage />} />
         <Route path="/documents/:documentId/extraction" element={<ExtractionResultsPage />} />
+
         <Route path="/tasks" element={<TasksPage />} />
         <Route path="/quality" element={<QualityPage />} />
         <Route path="/notifications" element={<NotificationsPage />} />

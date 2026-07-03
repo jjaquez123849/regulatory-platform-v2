ee7417ffe25b24e68eb633bdec039bb915355759
jjaquez123849 <joeljaqu123@gmail.com>
2026-07-03 13:12:18 -0400

    Update WorkspaceEngine.jsx

--- WorkspaceEngine.jsx
+++ WorkspaceEngine.jsx
@@ -3,10 +3,12 @@ import { useEffect } from "react";
 import WorkspaceProvider from "./WorkspaceProvider.jsx";
 import LayoutEngine from "./LayoutEngine.jsx";
 import { registerDefaultWidgets } from "./registerDefaultWidgets.js";
+import { registerDefaultWidgetManifests } from "./registerDefaultWidgetManifests.js";
 
 function WorkspaceEngine({ workspace }) {
   useEffect(() => {
     registerDefaultWidgets();
+    registerDefaultWidgetManifests();
   }, []);
 
   if (!workspace) {

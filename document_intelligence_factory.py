e258a507f5c2056febd7381fd75cd59e10dcaa75
jjaquez123849 <joeljaqu123@gmail.com>
2026-07-03 10:14:15 -0400

    Add function to get DocumentIntelligenceEngine instance

---
+++ document_intelligence_factory.py
@@ -0,0 +1,8 @@
+from app.engines.document_intelligence.document_intelligence_engine import (
+    DocumentIntelligenceEngine,
+)
+
+
+def get_document_intelligence_engine():
+
+    return DocumentIntelligenceEngine()

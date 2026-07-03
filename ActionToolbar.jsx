682441733fc98e15f1467f3eb278e1f272b0b0e1
jjaquez123849 <joeljaqu123@gmail.com>
2026-07-03 12:06:27 -0400

    Create ActionToolbar.jsx

---
+++ ActionToolbar.jsx
@@ -0,0 +1,11 @@
+import "./workspace.css";
+
+function ActionToolbar({ children }) {
+  return (
+    <div className="workspace-action-toolbar">
+      {children}
+    </div>
+  );
+}
+
+export default ActionToolbar;
